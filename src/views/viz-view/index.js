f8dde074f4a722c40f9887da10931a45e25f1a3a
John Osterman <josterman@pewtrusts.org>
2019-05-03 10:15:32 -0400

    Removes disabling of play button at end of manual 2008 animation; shows replay instead; wonkiness seems to be fix

--- src/views/viz-view/index.js
+++ src/views/viz-view/index.js
@@ -725,10 +725,13 @@ export default class VizView extends Element {
                             setTimeout(() => {
                                 this.enableYearButtons();
                                 console.log(S.getState('year')[0], this.model.years[this.model.years.length - 1], S.getState('year')[2]);
-                                if ( S.getState('year')[2] == 1 && !(S.getState('year')[0] == this.model.years[this.model.years.length - 1] && S.getState('year')[2] == 1 ) ){
+                                if ( S.getState('year')[2] == 1 ){
                                     this.enablePlayButton();
+                                    if ( S.getState('year')[0] == this.model.years[this.model.years.length - 1] ) {
+                                        this.showReplayOption();
+                                    }
                                 } 
-                               if ( !S.getState('isPaused') ){
+                                if ( !S.getState('isPaused') ){
                                     resolve(true);  
                                 } else {
                                     this.removePauseOption();
